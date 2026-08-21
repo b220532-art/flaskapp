68845137ad0071449f41bf16801a3949c6111790
b220532-art <b220532@rgukt.ac.in>
2026-08-21 10:14:40 +0000

    Update first.py

--- first.py
+++ first.py
@@ -2,8 +2,17 @@ from flask import Flask
 app =Flask(__name__)
 @app.route("/")
 def home():
-   return "flask application running successful"
+ 
+  # return "flask application running successful"
+   return """     
+       <html>
+   <body>
+      <h1>Hello RGUKT</h1>
+      </body> 
+</html>        
+
+
+   """
 if __name__==('__main__'):
         app.run('0.0.0.0',2000)
         # host,port
-        
